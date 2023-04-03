96c2f12966707e8386d095eb11ad178e9527a702
jjayxi <jasonbillard606@gmail.com>
2023-04-03 18:29:01 +0200

    add donut and plane

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use rayscii::*;
 use math_vector::Vector;
 
+use std::time::Instant;
+use std::f32::consts;
+
 mod camera;
 mod raymarcher;
 mod scene;
 use camera::Camera;
-use scene::Scene;
-use scene::Object;
-use scene::Sphere;
+use scene::*;
 use raymarcher::render;
 
 use std::{thread, time::Duration};
@@ -19,17 +20,28 @@ fn main() {
 
     let camera = Camera::new(Vector::new(0.0, 0.0, 0.0), 90.0, (screen.width, screen.height), 1.7);
     let mut scene_objs: Vec<Box<dyn Object>>= Vec::new();
-    scene_objs.push(Box::new(Sphere::new(Vector::new(0.0, 0.0, -80.0), 40.0)));
+    scene_objs.push(Box::new(Sphere::new(Vector::new(20.0, 10.0, -80.0), 40.0)));
+    scene_objs.push(Box::new(Plane::new(Vector::new(0.0, 30.0, 0.0), Vector::new(0.1, -1.0, 0.0))));
+    scene_objs.push(Box::new(Donut::new(Vector::new(-40.0, 0.0, -60.0), Vector::new(consts::PI / 4.0, 0.0, 0.0), 30.0, 7.0)));
     let mut scene = Scene::new(scene_objs);
 
     
 
     for _i in 0..300 {
-        render(&screen, &camera, &scene, 30, 100.0);
-        thread::sleep(Duration::from_millis(20));
+        let now = Instant::now();
+        render(&screen, &camera, &scene, 15, 100.0);
+        let elapsed = now.elapsed();
+        println!("Frame time: {:?}", elapsed);
+        
+        if elapsed < Duration::from_millis(120) {
+            let wait_time = Duration::from_millis(120) - elapsed;
+            thread::sleep(wait_time);
+        }
+        
         
         let sinv = ((_i as f32) / 10.0).cos() * 3.0;
 
         scene.objects[0].move_by(Vector::new(sinv, 0.0, 0.0));
+        scene.objects[2].rotate_by(Vector::new(0.1, 0.0, 0.0));
     }
 }
--- src/scene.rs
+++ src/scene.rs
@@ -24,6 +24,9 @@ impl Scene {
 pub trait Object {
     fn sdf(&self, point: Vector<f32>) -> f32;
     fn move_by(&mut self, by : Vector<f32>);
+    fn rotate_by(&mut self, _by : Vector<f32>) {
+        
+    }
 }
 
 pub struct Sphere {
@@ -35,7 +38,6 @@ impl Sphere {
         Sphere { position, radius }
     }
 }
-
 impl Object for Sphere {
     fn sdf(&self, point: Vector<f32>) -> f32 {
         (point - self.position).length() - self.radius
@@ -45,3 +47,58 @@ impl Object for Sphere {
         self.position = self.position + by;
     }
 }
+
+pub struct Plane {
+    pub position: Vector<f32>,
+    pub normal: Vector<f32>,
+}
+impl Plane {
+    pub fn new(position: Vector<f32>, normal: Vector<f32>) -> Plane {
+        Plane { position, normal }
+    }
+}
+impl Object for Plane {
+    fn sdf(&self, point: Vector<f32>) -> f32 {
+        Vector::dot(point - self.position, self.normal)
+    }
+
+    fn move_by(&mut self, _by : Vector<f32>) {
+        self.position = self.position + _by;
+    }
+}
+
+pub struct Donut {
+    pub position: Vector<f32>,
+    pub rotation: Vector<f32>,
+    pub radius: f32,
+    pub thickness: f32,
+}
+impl Donut {
+    pub fn new(position: Vector<f32>, rotation: Vector<f32>, radius: f32, thickness: f32) -> Donut {
+        Donut { position, rotation, radius, thickness}
+    }
+}
+
+impl Object for Donut {
+    fn sdf(&self, point: Vector<f32>) -> f32 {
+        let mut p = point - self.position;
+        p = p.rotate_x(self.rotation.x);
+        p = p.rotate_y(self.rotation.y);
+        p = p.rotate_z(self.rotation.z);
+        let mut q = Vector::new(p.x, p.y, 0.0);
+        q = q.normalize() * self.radius;
+        let mut d = (p - q).length() - self.thickness;
+        d = d.abs();
+        d
+    }
+
+    fn move_by(&mut self, by : Vector<f32>) {
+        self.position = self.position + by;
+    }
+
+    fn rotate_by(&mut self, by : Vector<f32>) {
+        self.rotation = self.rotation + by;
+    }
+}
+
+
